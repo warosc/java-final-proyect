0305e08237e9d6adfe5fa5bf2cd80ffaea618e7c
Harlan Giron <oscarestrada.giron@gmail.com>
2020-05-11 17:26:52 -0600

    entidades
    Se agrega emtidad para verificar las definiciones

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
 public class Main {
+    Scanner scan = new Scanner(System.in);
+    RandomAccessFile archivo = null;
+    private final String rutaArchivo = "";
 
 }
