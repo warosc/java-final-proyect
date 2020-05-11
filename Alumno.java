bfda38a4989b50acaca09458d631329a29b44714
Harlan Giron <oscarestrada.giron@gmail.com>
2020-05-11 13:17:03 -0600

    Se crea geters and seders de tributos de la entidad alumno

---
+++ Alumno.java
@@ -0,0 +1,84 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Alumno {
+    private final DateFormat formatoFecha = new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy");
+    private String nombre;
+    private Date fechaNacimiento;
+    private long carne;
+    private String telefono;
+    private double peso;
+    private byte[] bytesNombre;
+    private byte[] bytesTelefono;
+
+    public DateFormat getFormatoFecha() {
+        return formatoFecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+        bytesNombre = new  byte[100];
+        for (int i = 0; i<nombre.length();i++){
+            bytesNombre[i] = (byte)nombre.charAt(i);
+        }
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public long getCarne() {
+        return carne;
+    }
+
+    public void setCarne(long carne) {
+        this.carne = carne;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+        bytesTelefono = new byte[15];
+        for (int i = 0; i < telefono.length(); i++) {
+            bytesTelefono[i] = (byte) telefono.charAt(i);
+        }
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public byte[] getBytesNombre() {
+        return bytesNombre;
+    }
+
+    public void setBytesNombre(byte[] bytesNombre) {
+        this.bytesNombre = bytesNombre;
+        nombre = new String(bytesNombre);
+    }
+
+    public byte[] getBytesTelefono() {
+        return bytesTelefono;
+    }
+
+    public void setBytesTelefono(byte[] bytesTelefono) {
+        this.bytesTelefono = bytesTelefono;
+        telefono = new String(bytesTelefono);
+    }
+}
